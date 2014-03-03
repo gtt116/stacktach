d20973f52ee2f86078d7353a7cd9248ce2d6bdf8
gtt116 <gtt116@gmail.com>
2014-03-03 16:51:43 +0800

    Ignore some error of flake8

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-flake8 --exclude=".venv,./stacktach/migrations" .
+flake8 --exclude=".venv,./stacktach/migrations,./stacktach/settings.py" \
+    --ignore="E121,E122,E123,E124,E126,E127,E128,E711,E712,H102,H404,F403,F811,F841,H803" \
+    .
